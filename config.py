ab5ea099b4ea744b3963028169c78a0ce1880852
Sumit09988 <sumit999777555@gmail.com>
2026-04-13 14:33:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ BOT_TOKEN = "8654296227:AAHeQF2L8_a5rbxK9CdhXqzJLKx81sccNKo"
 ADMIN_ID = 7515864015
 BOT_USERNAME = "HARDSMSBOMBEROBOT"
 
-# ========== DONO CHANNEL COMPULSORY ==========
+# ========== DONO CHANNEL COMPULSORY (PUBLIC) ==========
 CHANNELS = [
     {"link": "https://t.me/SUMITNETW0RK", "username": "@SUMITNETW0RK", "name": "📢 MAIN CHANNEL"},
-    {"link": "https://t.me/+Exfa1Q5CgOxlNzdl", "username": "@LokiNetwork", "name": "🔥 LOKI NETWORK"}
+    {"link": "https://t.me/lokixnetwork", "username": "@lokixnetwork", "name": "🔥 LOKI NETWORK"}
 ]
 
 API_URL = "https://bomber-api-psi.vercel.app/send"
