6546b911961f3571e6d67ee855d5e2c5fc4375c2
Sumit09988 <sumit999777555@gmail.com>
2026-04-13 14:21:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,12 @@ BOT_TOKEN = "8654296227:AAHeQF2L8_a5rbxK9CdhXqzJLKx81sccNKo"
 ADMIN_ID = 7515864015
 BOT_USERNAME = "HARDSMSBOMBEROBOT"
 
-# ========== COMPULSORY CHANNEL (PUBLIC) ==========
-COMPULSORY_CHANNEL = {"link": "https://t.me/SUMITNETW0RK", "username": "@SUMITNETW0RK"}
-
-# ========== OPTIONAL CHANNELS (LINK SHOW HOGA, JOIN COMPULSORY NAHI) ==========
-OPTIONAL_CHANNELS = [
-    {"link": "https://t.me/+Exfa1Q5CgOxlNzdl", "name": "🔥 Loki Network", "type": "private"}
+# ========== DONO CHANNEL COMPULSORY ==========
+CHANNELS = [
+    {"link": "https://t.me/SUMITNETW0RK", "username": "@SUMITNETW0RK", "name": "📢 MAIN CHANNEL"},
+    {"link": "https://t.me/+Exfa1Q5CgOxlNzdl", "username": "@LokiNetwork", "name": "🔥 LOKI NETWORK"}
 ]
 
-GROUP_LINK = "https://t.me/+Exfa1Q5CgOxlNzdl"
 API_URL = "https://bomber-api-psi.vercel.app/send"
 API_KEY = "sumit996"
 DAILY_FREE = 5
